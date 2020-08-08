2e6f53a76cd8a284b77a5acb638c7995c945725c
Zach <zachchou016@gmail.com>
2020-08-08 14:05:50 +0800

    [add] first commit

---
+++ controller/operate.go
@@ -0,0 +1,5 @@
+package controller
+
+func ReturnVaule()string{
+	return "hi"
+}
---
+++ router/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"Zinx/controller"
+	"fmt"
+)
+
+func main(){
+	fmt.Println("hello go")
+	fmt.Println(controller.ReturnVaule())
+}
